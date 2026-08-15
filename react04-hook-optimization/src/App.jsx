05735f0efd887211b9de450d8869425f134a63dd
Dong-ik <ldigo@naver.com>
2026-08-15 15:02:30 +0900

    리엑트훅 유저메모

--- react04-hook-optimization/src/App.jsx
+++ react04-hook-optimization/src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import TopNavi from './components/TopNavi.jsx'
 import UseRefExam1 from './components/UseRefExam1.jsx'
 import UseRefExam2 from './components/UseRefExam2.jsx'
-//import UseMemo from './components/UseMemo.jsx'
+import UseMemoExam from './components/UseMemoExam.jsx'
 //import UseCallback from './components/UseCallback.jsx'
 //import UseId from './components/UseId.jsx'
 
@@ -15,7 +15,7 @@ function App() {
         <Route path="/" element={<UseRefExam1 />} />
         <Route path="/use-ref1" element={<UseRefExam1 />} />
         <Route path="/use-ref2" element={<UseRefExam2 />} />
-        {/* <Route path="/use-memo" element={<UseMemo />} /> */}
+        <Route path="/use-memo" element={<UseMemoExam />} />
         {/* <Route path="/use-callback" element={<UseCallback />} /> */}
         {/* <Route path="/use-id" element={<UseId />} /> */}
       </Routes>
